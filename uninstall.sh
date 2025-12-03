8ace15273df5b976c426c5637c3aad1f5445ce6a
spyhere <gekav3@mail.ru>
2025-12-03 12:32:59 +0200

    Make uninstall script safer. Check for wrapper before removing

--- uninstall.sh
+++ uninstall.sh
@@ -6,9 +6,23 @@ echo
 
 if [[ $ANSWER == "y" || $ANSWER == "Y" ]]; then
   echo "Uninstalling..."
-  rm "$HOME/.local/bin/tmtr"
-  cd .. && rm -rf tmtr
-  echo "tmtr has been uninstalled from your machine."
+  REPO_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
+  REPO_NAME="$(basename "$REPO_DIR")"
+  WRAPPER_LINK="$HOME/.local/bin/tmtr"
+
+  cd "$REPO_DIR/.."
+  rm -rf "$REPO_DIR"
+
+  if [[ -L WRAPPER_LINK ]]; then
+    rm "$WRAPPER_LINK"
+    echo "Removed wrapper $WRAPPER_LINK"
+  else
+    echo "No wrapper has been found"
+  fi
+
+  cd "$HOME"
+  echo "tmtr has been uninstalled from your machine"
+  echo "You have been moved to home directory"
 else
   echo "Aborting."
 fi
